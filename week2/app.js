763f9dc2d3c86b4cf81a0afbf0dea4e3fbf742e3
cammywan <cammywan0218@gmail.com>
2017-09-16 18:17:54 -0400

    Update app.js

--- week2/app.js
+++ week2/app.js
@@ -4,12 +4,12 @@ submit.onclick = function() {
     var adj1 = document.getElementById("adj1");
     var adj2 = document.getElementById("adj2");
     var color = document.getElementById("color");
-    var num = document.getElementById("num");
+    var clothes = document.getElementById("clothes");
     var country = document.getElementById("country");
     var noun = document.getElementById("noun");
     var adj3 = document.getElementById("adj3");
     var verb = document.getElementById("verb");
     var location = document.getElementById("location");
     var animal = document.getElementById("animal");
-    message.innerText = "Disney is planning on making a brand new and modern Snow White movie, with a new Prince, named " + name.value + ". He is a " + adj1.value + " and " + adj2.value + " man, with " + color.value + " hair. He is " + num.value + " feet tall and he is from " + country.value + " and loves " + noun.value + ". His motto is that life is too " + adj3.value + " to " + verb.value + " away. He met Snow White at " + location.value+ ". His spiritual animal is " + animal.value + "."; 
+    message.innerText = "Disney is planning on making a brand new and modern Snow White movie, with a new Prince, named " + name.value + ". He is a " + adj1.value + " and " + adj2.value + " man, with " + color.value + " hair. He always wears " + clothes.value + ". And he is from " + country.value + " and loves " + noun.value + ". His motto is that life is too " + adj3.value + " to " + verb.value + " away. He met Snow White at " + location.value+ ". His spiritual animal is " + animal.value + "."; 
 };
