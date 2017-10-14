87ec1be679a254720eb67c65f20eb4fb4fadbcbc
cammywan <cammywan0218@gmail.com>
2017-10-14 18:38:30 -0400

    Add files via upload

--- week5/script.js
+++ week5/script.js
@@ -3,29 +3,29 @@ submit.onclick = function() {
     var length = document.getElementById("length").value;
     var chars = "abcdefghijklmnopqrstuvwxyz1234567890";
     var suffix="";
-    
+    var str="";
     
     var a = Math.floor(Math.random() * 28);
    
     for (var i = 0; i<= keyword.length-1; i++){
         if (i%2 == 0){
-            keyword[i] = keyword[i].toUpperCase(); // change odd numbers characters of keyword to upper case
-            console.log(keyword[i]);
+           str += keyword.charAt(i).toUpperCase(); // change odd numbers characters of keyword to upper case
+        
         }
         else {
-            keyword[i] = keyword[i].toLowerCase();; // change even numbers characters of keyword to lower case
-            console.log(keyword[i]);
+           str +=  keyword.charAt(i).toLowerCase(); // change even numbers characters of keyword to lower case
+           
         }
     }
     
 
-    for (var i = 0; i<= Number(length) - keyword.length; i++){
+    for (var i = 0; i<= Number(length) - keyword.length-1; i++){
         suffix += chars[a+i];
     }
  // suffix is starting from a random char of chars and with a certain number of characters in a row
     console.log(keyword);
     console.log(length);
-    password.innerText =keyword + suffix;
+    password.innerText =str + suffix;
     
      
     
